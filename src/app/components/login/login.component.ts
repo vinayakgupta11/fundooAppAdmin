714498e63b79a69470cf2930b92451a095d28f9d
vinayak Gupta <vinayakgupta1107@gmail.com>
2019-10-11 18:50:53 +0530

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    $(document).ready(function(){
+      $("button").click(function(){
+        alert("Value: " + $("#email").val());
+      });
+    });
+  }
+
+
+
+}
